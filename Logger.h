53f5172713bea3cc5b80ab27991e19eb0ea9b49e
Filip Srajer <filip@srajer.eu>
2016-10-13 13:33:48 +0200

    Format description.

--- Logger.h
+++ Logger.h
@@ -6,6 +6,20 @@
 #include <fstream>
 #include <thread>
 
+using std::string;
+
+/**
+* Format is:
+* int32_t at file beginning for frame count
+*
+* For each frame:
+* int64_t: timestamp
+* int32_t: depthSize
+* int32_t: imageSize
+* depthSize * unsigned char: depth_compress_buf
+* imageSize * unsigned char: encodedImage->data.ptr
+*/
+
 class Logger
 {
 public:
